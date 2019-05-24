ca0ea9c81da34392b8a35e6dd761f052341374d1
Ajver <zawlockidominik@gmail.com>
2019-05-24 21:45:08 +0200

    Add gravity to arrow - test version

--- Scripts/Arrow.gd
+++ Scripts/Arrow.gd
@@ -2,7 +2,9 @@ extends KinematicBody2D
 
 var velocity
 export(float) var force
+export(float) var gravity
 
 func _physics_process(delta):
+	velocity.y += gravity * delta
 	var collision = move_and_collide(velocity * delta)
 	
--- Scripts/Elf.gd
+++ Scripts/Elf.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export(float) var arrow_speed;
+export(float) var min_arrow_speed;
 export(float) var next_arrow_wait_time
 
 var fire_point : Node2D
@@ -14,20 +14,30 @@ func _ready():
 func _process(delta):
 	next_arrow_timer -= delta
 	
-	if next_arrow_timer <= 0:
-		shot_arrow()
+	if next_arrow_timer > 0:
+		return
+		
+	var dwarf = $DwarfRayCast.get_collider()
+	
+	if not dwarf:
+		return
+		
+	var diff_x = dwarf.position.x - position.x
+	var force = max(diff_x, min_arrow_speed)
+	
+	shot_arrow(force)
 
 func _input(event):
 	if Input.is_action_just_pressed("faster_shot"):
 		next_arrow_timer -= 0.1
 				
-func shot_arrow():
+func shot_arrow(force):
 	restart_arrow_timer()
 	
 	var arrow = Arrow.instance()
 	get_parent().add_child(arrow);
 	arrow.global_position = fire_point.global_position
-	arrow.velocity = Vector2(arrow_speed, 0)
+	arrow.velocity = Vector2(force, 0)
 
 func restart_arrow_timer():
 	next_arrow_timer = next_arrow_wait_time
