51614084ac99c72a03b73456e203d5d0156fd709
Bozia <46607244+KazikBozia@users.noreply.github.com>
2019-06-17 19:57:12 +0200

    better offine screen

--- Scripts/OffineScreen.gd
+++ Scripts/OffineScreen.gd
@@ -7,11 +7,19 @@ onready var offine_time_label = find_node("OffineLabel")
 onready var reward_label = find_node("OffineLabel2")
 onready var game_data = get_parent().get_node("GameData")
 
+var time_to_end = 5.0
+
 func _ready():
 	if game_data.offine_time == 0:
 		queue_free()
 		pass
 	update_labels()
+
+func _process(delta):
+	time_to_end -= delta
+	if time_to_end > 0:
+		return
+	queue_free()
 
 func update_labels():
 	offine_time_label.text = offine_time_text + String(game_data.offine_time) + " sekund"
