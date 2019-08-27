0c46fbbf3c8c4147de3f463a0d968077c1d6d7c3
Bozia <46607244+KazikBozia@users.noreply.github.com>
2019-08-27 18:46:53 +0200

    correction

--- Scripts/StaminaSystem.gd
+++ Scripts/StaminaSystem.gd
@@ -1,6 +1,6 @@
 extends Label
 
-onready var stamina_label: Label = get_node(".")
+onready var stamina_label: Label = self
 onready var ElfStats = get_parent().get_parent().get_node("ElfStats")
 
 var current_stamina: float = 10
@@ -14,30 +14,20 @@ func _ready():
 	set_process_input(true)
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
+	if Input.is_action_just_pressed("increasing_stamina"):
 		increasing_stamina()
 
 func increasing_stamina():
-	if current_stamina < max_stamina:
-		if current_stamina + 1 > max_stamina:
-			current_stamina = max_stamina
-		else:
-			current_stamina += 1
-	else:
-		current_stamina = max_stamina
-	
+	current_stamina = min(current_stamina + 1, max_stamina)	
 
 func _process(delta):
 	if current_stamina > 0:
 		auto_atack = true
-		if current_stamina - delta < 0:
-			current_stamina = 0
-		else:
-			current_stamina -= delta
+		current_stamina = max(current_stamina - delta, 0)
 	else:
 		auto_atack = false
 	
 	max_stamina = ElfStats.get_stat("stamina").value
-	var status = String(stepify(current_stamina,0.1))+"s / " + String(max_stamina) + "s"
+	var status = String(stepify(current_stamina,0.1))+"s / " + String(stepify(max_stamina,0.1)) + "s"
 
 	stamina_label.stamina_label.text = status
