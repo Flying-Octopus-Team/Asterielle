9a7f441b34040103b76c25e46c4fed3bc8e43a1a
Bozia <46607244+KazikBozia@users.noreply.github.com>
2019-09-04 15:46:23 +0200

    Background

--- Scenes/World/Layer.gd
+++ Scenes/World/Layer.gd
@@ -46,11 +46,11 @@ func generate_tree() -> void:
 	add_child(tree)
 	
 	tree.position.x = -position.x + WINDOW_WIDTH + 200
-	tree.position.y = 550
+	tree.position.y = 520
 	
 	var texture = TREE_TEXTURES[randi() % TREE_TEXTURES.size()]
 	tree.texture = texture
 	
-	var random_scale: float = rand_range(-0.25,0.25);
+	var random_scale: float = rand_range(-0.35,0);
 	tree.scale = Vector2(tree_scale + random_scale, tree_scale + random_scale)
 	
