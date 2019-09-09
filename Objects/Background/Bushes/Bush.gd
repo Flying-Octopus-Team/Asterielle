400b620833ea3a9319e4f8a0afc3c04f781ce58a
Ajver <zawlockidominik@gmail.com>
2019-09-09 13:51:35 +0200

    Random bushes generating

---
+++ Objects/Background/Bushes/Bush.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func set_texture(texture:Texture) -> void:
+	$Sprite.texture = texture
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
+	print("Removed")
--- Objects/Background/Bushes/BushesSpawner.gd
+++ Objects/Background/Bushes/BushesSpawner.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 export(float) var move_speed
 export(float) var separation
+export(Array, Texture) var textures
+export(float) var deep_scale = 1.0
 
 var Bush = load("res://Objects/Background/Bushes/Bush.tscn")
 
@@ -13,11 +15,9 @@ onready var screen_width : float = get_viewport_rect().size.x
 func _ready() -> void:
 	randomize()
 	
-	return
-	
-	while position.x <= -screen_width:
+	while position.x >= -screen_width:
 		position.x -= separation + rand_range(-50.0, 50.0)
-		#spawn_next_bush()
+		spawn_next_bush()
 		
 	next_separation = separation + rand_range(-50.0, 50.0)
 
@@ -33,10 +33,10 @@ func _process(delta) -> void:
 func spawn_next_bush() -> void:
 	var bush = Bush.instance()
 	call_deferred("add_child", bush)
-	bush.position.x = screen_width + separation
-	bush.position.y = position.y
-	print("foo ", bush.position.x)
-	#bush.set_deferred("global_position.x", screen_width + separation)
-	#call_deferred("print", bush.global_position)
+	bush.position.x = -position.x + screen_width + separation
+	bush.set_texture(get_random_texture())
+	bush.scale = Vector2(deep_scale, deep_scale)
 	
-	
+func get_random_texture() -> Texture:
+	var index = randi() % textures.size()
+	return textures[index]
