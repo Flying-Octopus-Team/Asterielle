2bc8a95eb03601b758e0566308e97d1d089cac09
Ajver <zawlockidominik@gmail.com>
2019-10-13 14:12:05 +0200

    Divide DvarwesManager on 3 nodes + refactorisation

--- Objects/EventItem/EventManager.gd
+++ Objects/EventItem/EventManager.gd
@@ -1,29 +1,34 @@
 extends Node
 
+onready var timer = $Timer
+
 var dwarf_in_ballon: bool = false
-var timer = null
 var event_items : Array = [
 	load("res://Objects/EventItem/SackOfGold.tscn"),
 	load("res://Objects/EventItem/DwarfInBalloon.tscn")
 ]
 
 func _ready():
-	wait_to_spawn_item()
+	restart_timer()
 
-func wait_to_spawn_item():
-	timer = Timer.new()
+func restart_timer():
 	timer.set_wait_time(return_random_time())
-	timer.connect("timeout",self,"spawn_item")
-	add_child(timer)
 	timer.start()
 
 func return_random_time() -> float:
 	var value = randi()%40+20
 	return value
+	
+func _on_Timer_timeout():
+	spawn_item()
 
 func spawn_item():
 	var index : int = randi() % event_items.size()
 	var item = event_items[index].instance()
+	
+	restart_timer()
+	
 	if item.name == "DwarfInBalloon" && dwarf_in_ballon:
 		return
 	get_parent().call_deferred("add_child", item)
+
