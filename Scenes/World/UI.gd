d8125489b0717d6d9ffea20ffee3c9f6454ec9b8
Ajver <zawlockidominik@gmail.com>
2019-10-25 00:03:01 +0200

    Add icons to mail screen

--- Scenes/World/UI.gd
+++ Scenes/World/UI.gd
@@ -1,8 +1,15 @@
 extends Control
 
+onready var gold_icon = find_node("GoldIcon")
 onready var gold_label = find_node("GoldLabel")
+
+onready var xp_icon = find_node("XpIcon")
 onready var xp_label = find_node("XpLabel")
+
+onready var silver_moon_row = find_node("SilverMoonRow")
+onready var silver_moon_icon = find_node("SilverMoonIcon")
 onready var silver_moon_label = find_node("SilverMoonLabel")
+
 onready var level_label = find_node("LevelLabel")
 onready var killed_dwarves_label = find_node("KilledDwarvesLabel")
 onready var game_data = get_node("/root/World").find_node("GameData")
@@ -13,16 +20,20 @@ onready var revival_enter_btn = get_parent().find_node("RevivalEnterBtn")
 
 
 func set_gold_label(gold: float):
+	gold_icon.get_node("AnimationPlayer").play("gold_reached")
 	gold_label.text = str("Zloto: ", round(gold))
 	
 func set_xp_label(xp):
+	xp_icon.get_node("AnimationPlayer").play("xp_reached")
 	xp_label.text = str("Doswiadczenie: ", round(xp))
 
 func set_silver_moon_label(silver_moon):
 	if silver_moon > 0:
+		silver_moon_row.show()
+		silver_moon_icon.get_node("AnimationPlayer").play("silver_moon_reached")
 		silver_moon_label.text = str("Srebrne ksiezyce: ", silver_moon)
 	else:
-		silver_moon_label.text = ""
+		silver_moon_row.hide()
 
 func set_level_label(current_level:int):
 	level_label.text = str("Poziom ", String(current_level))
