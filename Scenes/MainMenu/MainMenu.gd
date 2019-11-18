03909004c3ec72ed084859a2735bec17df8f9c7c
B0ZIA <gbaglowczyk@gmail.com>
2019-11-18 14:34:17 +0100

    New game, exit, better buttons, bacground animation, labels

--- Scenes/MainMenu/MainMenu.gd
+++ Scenes/MainMenu/MainMenu.gd
@@ -4,3 +4,20 @@ var world_path = "res://Scenes/World/World.tscn"
 
 func _on_ContinueBtn_pressed():
 	get_tree().change_scene(world_path)
+
+
+func _on_NewGameBtn_pressed():
+	get_tree().change_scene(world_path)
+	get_tree().find_node("GameSaver").hard_reset()
+
+
+func _on_OptionsBtn_pressed():
+	pass # Replace with function body.
+
+
+func _on_AboutBtn_pressed():
+	pass # Replace with function body.
+
+
+func _on_ExitBtn_pressed():
+	get_tree().quit()
