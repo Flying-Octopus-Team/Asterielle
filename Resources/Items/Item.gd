818be883395227b6654a045d61f3aecfdd4d5c2f
Ajver <zawlockidominik@gmail.com>
2019-11-20 01:00:19 +0100

    Remove CHANGERS array from Item.gd

--- Resources/Items/Item.gd
+++ Resources/Items/Item.gd
@@ -13,7 +13,6 @@ export(Array, Resource) var stat_changers
 export(float) var price
 
 var QUALITY : Array
-var CHANGERS : Array
 
 var setuped : bool = false
 
@@ -26,7 +25,6 @@ func setup():
 	randomize()
 	
 	fill_quality_array()
-	fill_changers_array()
 
 func reset() -> void:
 	setup()
@@ -43,37 +41,24 @@ func fill_quality_array() -> void:
 		load("res://Resources/Items/ItemQuality/TypeLegendary.tres")
 	]
 	
-func fill_changers_array() -> void:
-	CHANGERS = [
-		load("res://Resources/Items/StatChangers/BowsKnowledgeChanger.tres"),
-		load("res://Resources/Items/StatChangers/AgilityChanger.tres"),
-		load("res://Resources/Items/StatChangers/VitalityChanger.tres"),
-		load("res://Resources/Items/StatChangers/CharismaChanger.tres"),
-		load("res://Resources/Items/StatChangers/EagleEyeChanger.tres"),
-		load("res://Resources/Items/StatChangers/MagicChanger.tres"),
-		load("res://Resources/Items/StatChangers/SensinitivePointsChanger.tres"),
-		load("res://Resources/Items/StatChangers/StrengthChanger.tres"),
-		load("res://Resources/Items/StatChangers/StaminaChanger.tres"),
-	]
-	
 func generate_random(lucky:float) -> void:
 	if QUALITY.empty():
 		fill_quality_array()
-	if CHANGERS.empty():
-		fill_changers_array()
 		
 	quality = get_random_element(QUALITY).duplicate()
 	stat_changers.clear()
 	
 	price = rand_range(1, 3) 
 	
+	var possible_stat_names := get_possible_stat_names_array()
+	
 	var no_changers = randi()%3 + 1
 	for i in range(no_changers):
-		stat_changers.push_back(pop_random_element(CHANGERS).duplicate())
-		stat_changers[i].generate_random()
-		stat_changers[i].item_name = name
-		
-	fill_changers_array()
+		var changer = StatChanger.new()
+		changer.stat_name = pop_random_element(possible_stat_names)
+		changer.item_name = name
+		changer.generate_random()
+		stat_changers.push_back(changer)
 	
 func get_random_element(from:Array):
 	return from[randi() % from.size()]
@@ -83,12 +68,13 @@ func pop_random_element(from:Array):
 	from.erase(element)
 	return element
 	
-func get_changer(stat_name:String) -> Resource:
-	for c in CHANGERS:
-		if c.stat_name == stat_name:
-			return c
-			
-	return null
+func get_possible_stat_names_array() -> Array:
+	var arr := []
+
+	for s in ElfStats.get_stats():
+		arr.push_back(s.name)
+	
+	return arr
 	
 func save():
 	var save_dict = {
@@ -110,6 +96,8 @@ func load_data(data):
 		
 	for key in changers:
 		var changer_data = changers[key] 
-		var new_changer = get_changer(changer_data["_stat_name"]).duplicate()
-		new_changer.load_data(changer_data)
-		stat_changers.push_back(new_changer)
+		var changer = StatChanger.new()
+		changer.stat_name = changer_data["_stat_name"]
+		changer.item_name = name
+		changer.load_data(changer_data)
+		stat_changers.push_back(changer)
