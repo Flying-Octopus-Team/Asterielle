5a72f3d5956b8a71dcafce133d7b0c016710fde3
B0ZIA <gbaglowczyk@gmail.com>
2019-11-20 19:37:58 +0100

    basic options

--- Scenes/MainMenu/MainMenu.gd
+++ Scenes/MainMenu/MainMenu.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var world_path = "res://Scenes/World/World.tscn"
 
+onready var home = get_node("Canvas/Home")
+onready var options = get_node("Canvas/Options")
+
+
+
 func _on_ContinueBtn_pressed():
 	get_tree().change_scene(world_path)
 
@@ -12,7 +17,7 @@ func _on_NewGameBtn_pressed():
 
 
 func _on_OptionsBtn_pressed():
-	pass # Replace with function body.
+	switch_panel(MENU_PANEL.OPTIONS)
 
 
 func _on_AboutBtn_pressed():
@@ -21,3 +26,21 @@ func _on_AboutBtn_pressed():
 
 func _on_ExitBtn_pressed():
 	get_tree().quit()
+
+func switch_panel(var menu_panel):
+	match menu_panel:
+		MENU_PANEL.HOME:
+			options.visible = false
+			home.visible = true
+			
+		MENU_PANEL.OPTIONS:
+			home.visible = false
+			options.visible = true
+
+enum MENU_PANEL{
+	HOME,
+	OPTIONS
+}
+
+func _on_BackBtn_pressed():
+	switch_panel(MENU_PANEL.HOME)
