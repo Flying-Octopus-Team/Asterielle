67f79ac2490ef7a03f9aad0ea7e06139f9157be5
B0ZIA <gbaglowczyk@gmail.com>
2019-12-02 15:12:41 +0100

    Cleaner mainMenu.gd

--- Scenes/MainMenu/MainMenu.gd
+++ Scenes/MainMenu/MainMenu.gd
@@ -1,35 +1,14 @@
 extends Node2D
 
-onready var WEBSITE_PATH = "http://elf-vs-dwarves.pl/"
-onready var SAVE_FILE_PATH = "save.json"
-
-onready var world_path = "res://Scenes/World/World.tscn"
+const WEBSITE_ADDRESS = "http://elf-vs-dwarves.pl"
+const PATH_TO_SAVE_FILE = "save.json"
+const WORLD_PATH = "res://Scenes/World/World.tscn"
 
 onready var home = get_node("Canvas/Home")
 onready var options = get_node("Canvas/Options")
 onready var about = get_node("Canvas/About")
 
 
-
-func _on_ContinueBtn_pressed():
-	get_tree().change_scene(world_path)
-
-
-func _on_NewGameBtn_pressed():
-	Directory.new().remove(SAVE_FILE_PATH)
-	get_tree().change_scene(world_path)
-
-
-func _on_OptionsBtn_pressed():
-	switch_panel(MENU_PANEL.OPTIONS)
-
-
-func _on_AboutBtn_pressed():
-	switch_panel(MENU_PANEL.ABOUT)
-
-
-func _on_ExitBtn_pressed():
-	get_tree().quit()
 
 func switch_panel(var menu_panel):
 	match menu_panel:
@@ -52,9 +31,24 @@ enum MENU_PANEL{
 	ABOUT
 }
 
+func _on_NewGameBtn_pressed():
+	Directory.new().remove(PATH_TO_SAVE_FILE)
+	get_tree().change_scene(WORLD_PATH)
+	
+func _on_ContinueBtn_pressed():
+	get_tree().change_scene(WORLD_PATH)
+
+func _on_OptionsBtn_pressed():
+	switch_panel(MENU_PANEL.OPTIONS)
+
+func _on_AboutBtn_pressed():
+	switch_panel(MENU_PANEL.ABOUT)
+
+func _on_ExitBtn_pressed():
+	get_tree().quit()
+
 func _on_BackBtn_pressed():
 	switch_panel(MENU_PANEL.HOME)
 
-
 func _on_LinkBtn_pressed():
-	OS.shell_open(WEBSITE_PATH) 
+	OS.shell_open(WEBSITE_ADDRESS) 
