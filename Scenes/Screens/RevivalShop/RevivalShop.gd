d5318022b18d7526969ce16a847a81b80f80a00a
Michal Biernacki <userofficeit@gmail.com>
2020-03-09 12:17:18 +0100

    Refactorize buttons
    Now, buttons will update itself and call upgrade from their code

--- Scenes/Screens/RevivalShop/RevivalShop.gd
+++ Scenes/Screens/RevivalShop/RevivalShop.gd
@@ -16,7 +16,7 @@ const BASIC_DAMAGE_PRCE = 0
 const BASIC_HP_PRCE = 0
 const BASIC_ITEMS_PRCE = 0
 
-
+"""
 onready var enemies_per_level_count = find_node("Item_enemies_per_level").find_node("Count")
 onready var earn_gold_count = find_node("Item_earn_gold").find_node("Count")
 onready var earn_xp_count = find_node("Item_earn_xp").find_node("Count")
@@ -35,36 +35,28 @@ onready var silver_moon_probability_button = find_node("Item_silver_moon_probabi
 onready var basic_start_level_button = find_node("Item_basic_start_level").find_node("Button")
 onready var basic_damage_button = find_node("Item_basic_damage").find_node("Button")
 onready var basic_hp_button = find_node("Item_basic_hp").find_node("Button")
-onready var items_price_button = find_node("Item_items_price").find_node("Button")
+onready var items_price_button = find_node("Item_items_price").find_node("Button")"""
 
 onready var level_manager = get_parent().find_node("LevelManager")
 
 func _ready():
-	connect_buttons_signals()
-	set_item_count(enemies_per_level_count, get_parent().find_node("LevelManager").dwarves_per_level)
-	set_item_button(enemies_per_level_button, '-', "return_enemies_per_level_access")
-	set_item_count(earn_gold_count, 'x' + String(GameData.additional_gold_multipler))
-	set_item_button(earn_gold_button, '+', "return_earn_gold_access")
-	set_item_count(earn_xp_count, 'x' + String(GameData.additional_xp_multipler))
-	set_item_button(earn_xp_button, '+', "return_earn_xp_access")
-	set_item_count(time_to_kill_boss_count, String(GameData.time_to_kill_boss) + 's')
-	set_item_button(time_to_kill_boss_button, '+', "return_time_to_kill_boss_access")
-	set_item_count(silver_moon_probability_count, String(GameData.probability_to_get_silver_moon_in_percent) + '%')
-	set_item_button(silver_moon_probability_button, '+', "return_silver_moon_probability_access")
-	set_item_count(basic_start_level_count, level_manager.basic_start_level)
-	set_item_button(basic_start_level_button, '+', "return_basic_start_level_access")
-	set_item_count(basic_damage_count, 'x' + String(ElfStats.damage_multiplier))
-	set_item_button(basic_damage_button, '+', "return_basic_damage_access")
-	set_item_count(basic_hp_count, 'x' + String(ElfStats.health_multiplier))
-	set_item_button(basic_hp_button, '+', "return_basic_hp_access")
-	set_item_count(items_price_count, 'x' + String(GameData.tradesman_item_price_multipler))
-	set_item_button(items_price_button, '+', "return_items_price_access")
+	#connect_buttons_signals()
+	send_count_text("Item_enemies_per_level", get_parent().find_node("LevelManager").dwarves_per_level)
+	send_count_text("Item_earn_gold", 'x' + String(GameData.additional_gold_multipler))
+	send_count_text("Item_earn_xp", 'x' + String(GameData.additional_xp_multipler))
+	send_count_text("Item_time_to_kill_boss", String(GameData.time_to_kill_boss) + 's')
+	send_count_text("Item_silver_moon_probability", String(GameData.probability_to_get_silver_moon_in_percent) + '%')
+	send_count_text("Item_basic_start_level", level_manager.basic_start_level)
+	send_count_text("Item_basic_damage", 'x' + String(ElfStats.damage_multiplier))
+	send_count_text("Item_basic_hp", 'x' + String(ElfStats.health_multiplier))
+	send_count_text("Item_items_price", 'x' + String(GameData.tradesman_item_price_multipler))
+
 
 func exit():
 	game_manager.resume_gameplay()
 	emit_signal("revival_shop_exit")
 	queue_free()
-
+"""
 func connect_buttons_signals():
 	enemies_per_level_button.connect("pressed", self, "upgrade_enemies_per_level")
 	earn_gold_button.connect("pressed", self, "upgrade_earn_gold")
@@ -74,14 +66,10 @@ func connect_buttons_signals():
 	basic_start_level_button.connect("pressed", self, "upgrade_basic_start_level")
 	basic_damage_button.connect("pressed", self, "upgrade_basic_damage")
 	basic_hp_button.connect("pressed", self, "upgrade_basic_hp")
-	items_price_button.connect("pressed", self, "upgrade_items_price")
-
-func set_item_button(button, text, return_func_name):
-	button.disabled = self.call(return_func_name)
-	button.text = text
+	items_price_button.connect("pressed", self, "upgrade_items_price")"""
 	
-func set_item_count(count, text):
-	count.text = String(text)
+func send_count_text(item_name, text):
+	find_node(item_name).set_count_string(String(text))
 
 
 func return_enemies_per_level_access() -> bool:
@@ -93,8 +81,7 @@ func return_enemies_per_level_access() -> bool:
 func upgrade_enemies_per_level():
 	level_manager.dwarves_per_level -= 1
 	get_parent().find_node("UIContainer").set_killed_dwarves_label(level_manager.killed_dwarves, level_manager.dwarves_per_level)
-	set_item_count(enemies_per_level_count, get_parent().find_node("LevelManager").dwarves_per_level)
-	set_item_button(enemies_per_level_button, '-', "return_enemies_per_level_access")
+	send_count_text("Item_enemies_per_level", get_parent().find_node("LevelManager").dwarves_per_level)
 
 
 func return_earn_gold_access() -> bool:
@@ -103,8 +90,7 @@ func return_earn_gold_access() -> bool:
 
 func upgrade_earn_gold():
 	GameData.additional_gold_multipler += 0.1
-	set_item_count(earn_gold_count, 'x' + String(GameData.additional_gold_multipler))
-	set_item_button(earn_gold_button, '+', "return_earn_gold_access")
+	send_count_text("Item_earn_gold", 'x' + String(GameData.additional_gold_multipler))
 
 
 func return_earn_xp_access() -> bool:
@@ -113,8 +99,7 @@ func return_earn_xp_access() -> bool:
 
 func upgrade_earn_xp():
 	GameData.additional_xp_multipler += 0.1
-	set_item_count(earn_xp_count, 'x' + String(GameData.additional_xp_multipler))
-	set_item_button(earn_xp_button, '+', "return_earn_xp_access")
+	send_count_text("Item_earn_xp", 'x' + String(GameData.additional_xp_multipler))
 
 
 func return_time_to_kill_boss_access() -> bool:
@@ -123,8 +108,7 @@ func return_time_to_kill_boss_access() -> bool:
 
 func upgrade_time_to_kill_boss():
 	GameData.time_to_kill_boss += 5
-	set_item_count(time_to_kill_boss_count, String(GameData.time_to_kill_boss) + 's')
-	set_item_button(time_to_kill_boss_button, '+', "return_time_to_kill_boss_access")
+	send_count_text("Item_time_to_kill_boss", String(GameData.time_to_kill_boss) + 's')
 
 
 func return_silver_moon_probability_access() -> bool:
@@ -135,8 +119,7 @@ func return_silver_moon_probability_access() -> bool:
 
 func upgrade_silver_moon_probability():
 	GameData.probability_to_get_silver_moon_in_percent += 5
-	set_item_count(silver_moon_probability_count, String(GameData.probability_to_get_silver_moon_in_percent) + '%')
-	set_item_button(silver_moon_probability_button, '+', "return_silver_moon_probability_access")
+	send_count_text("Item_silver_moon_probability", String(GameData.probability_to_get_silver_moon_in_percent) + '%')
 
 
 func return_basic_start_level_access() -> bool:
@@ -146,8 +129,7 @@ func return_basic_start_level_access() -> bool:
 func upgrade_basic_start_level():
 	level_manager.basic_start_level += 5
 	level_manager.current_level = level_manager.basic_start_level
-	set_item_count(basic_start_level_count, level_manager.basic_start_level)
-	set_item_button(basic_start_level_button, '+', "return_basic_start_level_access")
+	send_count_text("Item_basic_start_level", level_manager.basic_start_level)
 
 
 func return_basic_damage_access() -> bool:
@@ -156,8 +138,7 @@ func return_basic_damage_access() -> bool:
 
 func upgrade_basic_damage():
 	ElfStats.damage_multiplier += 0.1
-	set_item_count(basic_damage_count, 'x' + String(ElfStats.damage_multiplier))
-	set_item_button(basic_damage_button, '+', "return_basic_damage_access")
+	send_count_text("Item_basic_damage", 'x' + String(ElfStats.damage_multiplier))
 
 
 func return_basic_hp_access() -> bool:
@@ -166,8 +147,7 @@ func return_basic_hp_access() -> bool:
 
 func upgrade_basic_hp():
 	ElfStats.health_multiplier += 0.1
-	set_item_count(basic_hp_count, 'x' + String(ElfStats.health_multiplier))
-	set_item_button(basic_hp_button, '+', "return_basic_hp_access")
+	send_count_text("Item_basic_hp", 'x' + String(ElfStats.health_multiplier))
 
 
 func return_items_price_access() -> bool:
@@ -176,5 +156,4 @@ func return_items_price_access() -> bool:
 	
 func upgrade_items_price():
 	GameData.tradesman_item_price_multipler += 0.1
-	set_item_count(items_price_count, 'x' + String(GameData.tradesman_item_price_multipler))
-	set_item_button(items_price_button, '+', "return_items_price_access")
+	send_count_text("Item_items_price", 'x' + String(GameData.tradesman_item_price_multipler))
--- Scenes/Screens/RevivalShop/RevivalShopItem.gd
+++ Scenes/Screens/RevivalShop/RevivalShopItem.gd
@@ -2,7 +2,25 @@ extends HBoxContainer
 
 export (Color, RGB) var color
 export (String) var info
+export (String) var return_func
+export (String) var upgrade_func
+export (String) var button_sign
+var count_text
+
 
 func _ready():
 	$Info.set_text(info)
 	$Count.set("custom_colors/font_color", color)
+	
+func update():
+	$Button.disabled = get_owner().call(return_func)
+	$Button.text = button_sign
+	$Count.text = String(count_text)
+	
+func set_count_string(s):
+	count_text = String(s)
+	update()
+
+func _on_Button_pressed():
+	get_owner().call(upgrade_func)
+	update()
