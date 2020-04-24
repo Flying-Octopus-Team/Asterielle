326e7a2562263174cf3f9dd9164d0cfc9d760e14
Michal Biernacki <userofficeit@gmail.com>
2020-04-25 01:30:42 +0200

    Shorten tavern and devil coming time
    Shortened time in which devil and tavern arrives. I added function to BackgroundData which changes its speed when neccesary.

--- Objects/Background/BackgroundData.gd
+++ Objects/Background/BackgroundData.gd
@@ -1,3 +1,18 @@
 extends Node
 
+var normal_speed: float = 70
+var fast_speed: float = 140
+
 var move_speed: float = 70
+
+func change_to_fast_speed():
+	move_speed = fast_speed
+	var back_to_normal_timer = Timer.new()
+	back_to_normal_timer.connect("timeout", self, "change_back_to_normal")
+	back_to_normal_timer.set_name("Timer")
+	add_child(back_to_normal_timer)
+	back_to_normal_timer.start(4)
+	
+func change_back_to_normal():
+	move_speed = normal_speed
+	$Timer.queue_free()
