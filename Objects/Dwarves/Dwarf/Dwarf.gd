ad9a9c4614c72a67563d2c59615b17285f13c015
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2020-07-19 20:30:35 +0200

    Add dwarves simple animatio system, adjust spritesheets add walking animations

--- Objects/Dwarves/Dwarf/Dwarf.gd
+++ Objects/Dwarves/Dwarf/Dwarf.gd
@@ -6,19 +6,35 @@ signal died
 
 export(float) var move_speed_mod = 1
 
-var DWARVES_TEXTURES = [
-	load("res://Objects/Dwarves/Dwarf/dwarf.png"),
-	load("res://Objects/Dwarves/Dwarf/kamikazekrasnal.png"),
-	load("res://Objects/Dwarves/Dwarf/kransoludtarcz_kopia.png"),
-	load("res://Objects/Dwarves/Dwarf/krasnolud_mag.png"),
-	load("res://Objects/Dwarves/Dwarf/krasnoludkilof.png"),
-	load("res://Objects/Dwarves/Dwarf/strzelec.png"),
-	load("res://Objects/Dwarves/Dwarf/ubogikrasnolud.png")
-]
+const POSITION_KEY : String = "POSITION"
+const WALKING_ANIM_KEY : String = "WALKING_ANIMATION_KEY"
+
+enum DwarfType {
+	DEFAULT,
+	KAMIKAZE,
+	SHIELD,
+	MAGE,
+	PICKAXE,
+	SHOOTER,
+	POOR
+}
+
+var DWARVES_PRESETS = {
+	DwarfType.DEFAULT: 	{ POSITION_KEY: Vector2(0, -23), WALKING_ANIM_KEY: "basic"},
+	DwarfType.KAMIKAZE: { POSITION_KEY: Vector2(0, -25), WALKING_ANIM_KEY: "kamikaze"},
+	DwarfType.SHIELD: 	{ POSITION_KEY: Vector2(0, -23), WALKING_ANIM_KEY: "shield"},
+	DwarfType.MAGE: 	{ POSITION_KEY: Vector2(0, -25), WALKING_ANIM_KEY: "mage"},
+	DwarfType.PICKAXE: 	{ POSITION_KEY: Vector2(0, -31), WALKING_ANIM_KEY: "pickaxe"},
+	DwarfType.SHOOTER: 	{ POSITION_KEY: Vector2(0, -31), WALKING_ANIM_KEY: "shooter"},
+	DwarfType.POOR: 	{ POSITION_KEY: Vector2(0, -24), WALKING_ANIM_KEY: "poor"}
+}
 
 var velocity : Vector2
 var hp : float
 var damage : float
+
+#should one of dwarf presets
+var dwarf_preset
 
 onready var hp_bar
 onready var hp_label
@@ -26,11 +42,16 @@ onready var hp_label
 onready var next_attack_timer : Timer = $NextAttackTimer
 onready var die_sound : AudioStreamPlayer = $DieSound
 onready var attack_sound : AudioStreamPlayer = $AttackSound
+onready var animated_sprite : AnimatedSprite = $AnimatedSprite
 
 func _ready():
+	choose_random_dwarf_type()
 	add_to_group("IDwarf")
 	go_forward()
-	set_texture()
+	
+func choose_random_dwarf_type():
+	dwarf_preset = DWARVES_PRESETS[randi() % DWARVES_PRESETS.size()]
+	animated_sprite.position = dwarf_preset[POSITION_KEY]
 	
 func set_data(new_hp, new_damage) -> void:
 	set_hp(new_hp)
@@ -45,10 +66,6 @@ func set_hp(new_hp):
 	hp_bar.value = hp
 	hp_label.text = str(stepify(hp,0.01))
 
-func set_texture():
-	var texture = DWARVES_TEXTURES[randi() % DWARVES_TEXTURES.size()]
-	find_node("Sprite").texture = texture
-
 func _physics_process(delta):
 	position += velocity * delta
 		
@@ -57,6 +74,7 @@ func _physics_process(delta):
 		_play_attack_sound()
 		velocity = Vector2.ZERO
 		set_physics_process(false)
+		animated_sprite.stop()
 		BackgroundData.move_speed = 0
 	
 func on_arrow_hit(arrow):
@@ -75,6 +93,7 @@ func on_arrow_hit(arrow):
 	
 func go_forward():
 	velocity = Vector2(-move_speed_mod * BackgroundData.move_speed, 0)
+	animated_sprite.play(dwarf_preset[WALKING_ANIM_KEY])
 	
 func _on_NextAttackTimer_timeout():
 	attack()
@@ -106,6 +125,7 @@ func death():
 	
 	# Stop moving
 	set_physics_process(false)
+	animated_sprite.stop()
 	
 	# Placeholder (animation in future)
 	hide()
